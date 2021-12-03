2793f80967258b04d4815c3bd35ace43fd127af3
Andrew Goodridge <gandrew888@gmail.com>
2021-12-02 23:51:46 -0500

    🅰️ Made some progress on the hero!

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,18 +1,25 @@
-import * as React from "react"
+import React, { useState, useEffect } from "react"
 
 import '../styles/hero.scss'
 
 const Hero = () => {
 
+    const [perspective, setPerspective] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => setPerspective(true), 1500);
+      });
+
     return (
         <main className="hero-container">
+            <p className={perspective ? "hero-perspectiveP" : ""}>(Logo Animation)</p>
             <div className="hero-background">
                 <div className="hero-top"></div>
                 <div className="hero-right"></div>
                 <div className="hero-bottom"></div>
                 <div className="hero-left"></div>
             </div>
-            <div className="hero-back"></div>
+            <div className={perspective ? "hero-back hero-perspectiveBack" : "hero-back"}></div>
         </main>
     )
 }
