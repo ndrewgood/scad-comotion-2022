931f7c6df508277343450a9d9135dff4214e942f
Varun Khatri <varunmk05@gmail.com>
2021-12-06 19:32:41 -0500

    nav exploration

--- src/components/nav.js
+++ src/components/nav.js
@@ -9,11 +9,29 @@ const Nav = ({context}) => {
         <nav className={"nav-container " + context}>
             <Link to="/">Home</Link>
             <div className="nav-right">
-                <Link to="/">Schedule</Link>
+
+            <div class="box show-front">
+                <div class="box__face box__face--front">Link</div>
+                <div class="box__face box__face--back"></div>
+                <div class="box__face box__face--right"></div>
+                <div class="box__face box__face--left"></div>
+                <div class="box__face box__face--top"></div>
+                <div class="box__face box__face--bottom"></div>
+            </div>
+                {/* <Link to="/">Schedule</Link>
                 <Link to="/faq">FAQ</Link>
-                <a href="https://society6.com/momelove">Store</a>
+                <a href="https://society6.com/momelove">Store</a> */}
             </div>
         </nav>
+
+        // <nav className={"nav-container " + context}>
+        //     <Link to="/">Home</Link>
+        //     <div className="nav-right">
+        //         <Link to="/">Schedule</Link>
+        //         <Link to="/faq">FAQ</Link>
+        //         <a href="https://society6.com/momelove">Store</a>
+        //     </div>
+        // </nav>
     )
 }
 
