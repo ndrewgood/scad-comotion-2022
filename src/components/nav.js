efa879f00a6405292fdd1fc1423eed2b6e3ac32c
Varun Khatri <varunmk05@gmail.com>
2021-12-07 15:59:51 -0500

    nav progress

--- src/components/nav.js
+++ src/components/nav.js
@@ -9,15 +9,36 @@ const Nav = ({context}) => {
         <nav className={"nav-container " + context}>
             <Link to="/">Home</Link>
             <div className="nav-right">
-
-            <div class="box show-front">
-                <div class="box__face box__face--front">Link</div>
-                <div class="box__face box__face--back"></div>
-                <div class="box__face box__face--right"></div>
-                <div class="box__face box__face--left"></div>
-                <div class="box__face box__face--top"></div>
-                <div class="box__face box__face--bottom"></div>
-            </div>
+                <div className="wall">
+                    <div className="leftWall"></div>
+                </div>
+                <Link className="box bring-forward" to="/">
+                    <div className="box__face box__face--front">link</div>
+                    <div className="box__face box__face--back"></div>
+                    <div className="box__face box__face--right"></div>
+                    <div className="box__face box__face--left"></div>
+                    <div className="box__face box__face--top"></div>
+                    <div className="box__face box__face--bottom"></div>
+                </Link>
+                <Link className="box bring-forward" to="/">
+                    <div className="box__face box__face--front">link</div>
+                    <div className="box__face box__face--back"></div>
+                    <div className="box__face box__face--right"></div>
+                    <div className="box__face box__face--left"></div>
+                    <div className="box__face box__face--top"></div>
+                    <div className="box__face box__face--bottom"></div>
+                </Link>
+                <Link className="box bring-back" to="/">
+                    <div className="box__face box__face--front">link</div>
+                    <div className="box__face box__face--back"></div>
+                    <div className="box__face box__face--right"></div>
+                    <div className="box__face box__face--left"></div>
+                    <div className="box__face box__face--top"></div>
+                    <div className="box__face box__face--bottom"></div>
+                </Link>
+                <div className="wall">
+                    <div className="rightWall"></div>
+                </div>
                 {/* <Link to="/">Schedule</Link>
                 <Link to="/faq">FAQ</Link>
                 <a href="https://society6.com/momelove">Store</a> */}
