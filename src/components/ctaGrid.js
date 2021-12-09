2030f77f3e494d76122028f2c253349aa80e7179
Andrew Goodridge <gandrew888@gmail.com>
2021-12-09 01:30:05 -0500

    Updated responsiveness on CTA and hero

--- src/components/ctaGrid.js
+++ src/components/ctaGrid.js
@@ -6,9 +6,11 @@ import '../styles/ctaGrid.scss'
 const CtaGrid = () => {
 
     return (
-        <main className="ctaGrid-container">
-            <Link to="/attendees" className="cta bg1"><p>Attendees Page</p></Link>
-            <Link to="/faq" className="cta bg2"><p>FAQ Page</p></Link>
+        <main className="ctaGrid">
+            <div className="ctaGrid-container">
+                <Link to="/attendees" className="cta bg1"><p>Attendees Page</p></Link>
+                <Link to="/faq" className="cta bg2"><p>FAQ Page</p></Link>
+            </div>
         </main>
     )
 }
