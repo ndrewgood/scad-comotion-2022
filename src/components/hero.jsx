b6457d436a8e7ef8ed5f666c87dc6eeccb6e0d18
Andrew Goodridge <gandrew888@gmail.com>
2021-12-09 01:14:32 -0500

    Added multiple hooks, started new spheres in hero

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,18 +1,58 @@
 import React, { useState, useEffect } from "react"
+import { useMousePosition } from "../util/useMousePosition"; 
+import { useWindowSize } from '../util/useWindowDimensions';
+import { useScrollPosition } from "../util/useScrollPosition";
+import { v4 as uuidv4 } from 'uuid';
+
 
 import '../styles/hero.scss'
 
+import Logo from '../assets/images/still-logo.png'
+
+// https://stackoverflow.com/questions/45363008/how-can-i-detect-when-video-finished-playing-react 
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
 const Hero = () => {
 
+    const delay = 1500;
+    const mouse = useMousePosition();
+    // console.log(mouse.x + ", " + mouse.y);
+    const [shapeList, setShapeList] = useState([]);
+    const scroll = useScrollPosition();
+    const size = useWindowSize();
+    // console.log(size.width + ", " + size.height);
+
+    const handleMouseClick = () => {
+        if(mouse.x < size.width - 30 && mouse.x > 30 && mouse.y > 30 && mouse.y < size.height - 30) {
+            setShapeList([...shapeList, {x: mouse.x + scroll.x, y: mouse.y + scroll.y, key: uuidv4()}]);
+        }
+
+    }
+
     const [perspective, setPerspective] = useState(false);
+    const [perspectiveFinished, setPerspectiveFinished] = useState(false);
 
     useEffect(() => {
-        setTimeout(() => setPerspective(true), 1500);
-      });
+        setTimeout(() => setPerspective(true), delay);
+        setTimeout(() => setPerspectiveFinished(true), delay + 1300);
+
+    }, []);
+
+
 
     return (
-        <main className="hero-container">
-            <p className={perspective ? "hero-perspectiveP" : ""}>(Logo Animation)</p>
+        <main className="hero-container" onClick={() => handleMouseClick()}>
+            <div className="hero-ball-container">
+            {
+                shapeList.map(shape => {
+                    return <div key={shape.key} className="hero-ball" style={{top: shape.y + "px", left: shape.x + "px"}}></div>
+                })
+            }
+            </div>
+            <img src={Logo} alt="CoMotion 2022 Logo" className={perspective ? "hero-perspectiveImg" : ""} />
             <div className="hero-background">
                 <div className="hero-top"></div>
                 <div className="hero-right"></div>
---
+++ src/util/useScrollPosition.js
@@ -0,0 +1,31 @@
+   
+import { useState, useEffect } from 'react';
+
+const isBrowser = typeof window !== `undefined`;
+
+function getScrollPosition() {
+  return isBrowser ? { x: window.pageXOffset, y: window.pageYOffset } : { x: 0, y: 0 };
+}
+
+export function useScrollPosition() {
+  const [position, setScrollPosition] = useState(getScrollPosition());
+
+  useEffect(() => {
+    let requestRunning = null;
+    function handleScroll() {
+      if (isBrowser && requestRunning === null) {
+        requestRunning = window.requestAnimationFrame(() => {
+          setScrollPosition(getScrollPosition());
+          requestRunning = null;
+        });
+      }
+    }
+
+    if (isBrowser) {
+      window.addEventListener('scroll', handleScroll);
+      return () => window.removeEventListener('scroll', handleScroll);
+    }
+  }, []);
+
+  return position;
+}
