ec1d530402d50b36653f58e86c92cd5f776ba521
Varun Khatri <varunmk05@gmail.com>
2021-12-15 15:43:12 -0500

    merge

--- src/components/nav.js
+++ src/components/nav.js
@@ -12,30 +12,30 @@ const Nav = ({context}) => {
                 <div className="wall">
                     <div className="leftWall"></div>
                 </div>
-                <Link className="box bring-forward pattern-1" to="/">
+                <Link className="box bring-forward pattern-1" to="/schedule">
                     <div className="box__face box__face--front">Schedule</div>
                     <div className="box__face box__face--back"></div>
                     <div className="box__face box__face--right"></div>
                     <div className="box__face box__face--left"></div>
-                    <div className="box__face box__face--top"></div>
+                    <div className="box__face box__face--top">Schedule</div>
                     <div className="box__face box__face--bottom"></div>
                 </Link>
-                <Link className="box bring-forward pattern-2" to="/">
+                <Link className="box bring-forward pattern-2" to="/faq">
                     <div className="box__face box__face--front">FAQ</div>
                     <div className="box__face box__face--back"></div>
                     <div className="box__face box__face--right"></div>
                     <div className="box__face box__face--left"></div>
-                    <div className="box__face box__face--top"></div>
+                    <div className="box__face box__face--top">FAQ</div>
                     <div className="box__face box__face--bottom"></div>
                 </Link>
-                <Link className="box bring-back pattern-3" to="/">
+                <a className="box bring-back pattern-3" href="https://society6.com/momelove">
                     <div className="box__face box__face--front">Store</div>
                     <div className="box__face box__face--back"></div>
                     <div className="box__face box__face--right"></div>
                     <div className="box__face box__face--left"></div>
-                    <div className="box__face box__face--top"></div>
+                    <div className="box__face box__face--top">Store</div>
                     <div className="box__face box__face--bottom"></div>
-                </Link>
+                </a>
                 <div className="wall">
                     <div className="rightWall"></div>
                 </div>
