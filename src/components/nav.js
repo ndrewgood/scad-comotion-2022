1e244441b60ad9731608cea8f60c5fce48d1e0c9
Varun Khatri <varunkhatri@Varuns-MacBook-Pro.local>
2021-12-16 11:08:04 -0500

    desktop nav complete

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'gatsby'
+import Logo from '../assets/images/still-logo.png'
 
 import '../styles/nav.scss'
 
@@ -31,42 +32,48 @@ const Nav = ({context}) => {
 
 
     return (
-        <nav className={"nav-container " + context}>
-            {sticky ? <Link to="/">Home</Link> : null}
-            <div className="nav-right">
-                <div className="wall">
-                    <div className="leftWall"></div>
+        <nav className={sticky ?  "sticky": null}>
+            <div className={sticky ? "nav-container sticky " + context : "nav-container " + context}>
+                <div className={sticky ?  "homeLink": "homeLink hidden"}>
+                    <Link to="/">
+                        <img src={Logo} alt="Comotion 2022 logo"/>
+                    </Link>
                 </div>
-                <Link className="box bring-forward pattern-1" to="/schedule">
-                    <div className="box__face box__face--front">Schedule</div>
-                    <div className="box__face box__face--back"></div>
-                    <div className="box__face box__face--right"></div>
-                    <div className="box__face box__face--left"></div>
-                    <div className="box__face box__face--top">Schedule</div>
-                    <div className="box__face box__face--bottom"></div>
-                </Link>
-                <Link className="box bring-forward pattern-2" to="/faq">
-                    <div className="box__face box__face--front">FAQ</div>
-                    <div className="box__face box__face--back"></div>
-                    <div className="box__face box__face--right"></div>
-                    <div className="box__face box__face--left"></div>
-                    <div className="box__face box__face--top">FAQ</div>
-                    <div className="box__face box__face--bottom"></div>
-                </Link>
-                <a className="box bring-back pattern-3" href="https://society6.com/momelove">
-                    <div className="box__face box__face--front">Store</div>
-                    <div className="box__face box__face--back"></div>
-                    <div className="box__face box__face--right"></div>
-                    <div className="box__face box__face--left"></div>
-                    <div className="box__face box__face--top">Store</div>
-                    <div className="box__face box__face--bottom"></div>
-                </a>
-                <div className="wall">
-                    <div className="rightWall"></div>
+                <div className="nav-right">
+                    <div className="wall">
+                        <div className="leftWall"></div>
+                    </div>
+                    <Link className="box bring-forward pattern-1" to="/schedule">
+                        <div className="box__face box__face--front">Schedule</div>
+                        <div className="box__face box__face--back"></div>
+                        <div className="box__face box__face--right"></div>
+                        <div className="box__face box__face--left"></div>
+                        <div className="box__face box__face--top">Schedule</div>
+                        <div className="box__face box__face--bottom"></div>
+                    </Link>
+                    <Link className="box bring-forward pattern-2" to="/faq">
+                        <div className="box__face box__face--front">FAQ</div>
+                        <div className="box__face box__face--back"></div>
+                        <div className="box__face box__face--right"></div>
+                        <div className="box__face box__face--left"></div>
+                        <div className="box__face box__face--top">FAQ</div>
+                        <div className="box__face box__face--bottom"></div>
+                    </Link>
+                    <a className="box bring-back pattern-3" href="https://society6.com/momelove">
+                        <div className="box__face box__face--front">Store</div>
+                        <div className="box__face box__face--back"></div>
+                        <div className="box__face box__face--right"></div>
+                        <div className="box__face box__face--left"></div>
+                        <div className="box__face box__face--top">Store</div>
+                        <div className="box__face box__face--bottom"></div>
+                    </a>
+                    <div className="wall">
+                        <div className="rightWall"></div>
+                    </div>
+                    {/* <Link to="/">Schedule</Link>
+                    <Link to="/faq">FAQ</Link>
+                    <a href="https://society6.com/momelove">Store</a> */}
                 </div>
-                {/* <Link to="/">Schedule</Link>
-                <Link to="/faq">FAQ</Link>
-                <a href="https://society6.com/momelove">Store</a> */}
             </div>
         </nav>
 
