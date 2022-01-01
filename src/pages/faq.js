b469d5c476bb22f0cd2e04eb5c33180454cf28cd
Andrew Goodridge <gandrew888@gmail.com>
2021-12-31 19:11:26 -0500

    fixed small things

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -23,8 +23,9 @@ const FaqComponent = ({question, answer, deg}) => {
       <div className="faq-answer">
         {
           answer.map((node) => {
-            if (node == "br") {
-              return <br />
+            if (typeof node === "object") {
+              return <p>{node[0]}<a href={node[1][1]}>{node[1][0]}</a>{node[2]}</p>
+              // return <p>{node}</p>
             } else {
               return <p>{node}</p>
             }
@@ -42,23 +43,40 @@ const FaqPage = () => (
     <FaqHero />
     <div style={{backgroundColor: "#FFDCDF", padding: "40px 0"}}>
       <FaqComponent 
-        question="Is CoMotion going to be held virtually this year?"
-        answer={["Yes! CoMotion 2021 will be the first all-virtual CoMotion. Everything will be accessible from our website, scadcomotion.com."]}
+        question="Is CoMotion virtual or on-ground this year and where are livestreams hosted?"
+        answer={[
+          "CoMotion 2022 is the second fully virtual CoMotion. All livestreamed events are accessible on YouTube through the ‘Livestreams’ section on our website, scadcomotion.com."
+        ]}
         deg={-1}
       />
       <FaqComponent 
-        question="When and where are my Portfolio Reviews? With whom am I reviewing?"
-        answer={["Everyone who was selected for the virtual Portfolio Reviews during CoMotion 2021 should have received an email from MOMELove with the subject line: 'CoMotion 2021 Portfolio Review.' This email will have your review schedule as well as helpful information regarding the review process."]}
+        question="How are virtual Portfolio Reviews hosted during CoMotion 2022?"
+        answer={[
+          [
+            "Portfolio reviews will be conducted via the CoMotion 2022 Discord server. Company attendees can access this server via their itinerary email sent by MOMELove, and students can access this through the MOMELove Club Discord. If you are new to Discord, you can make a free account and look through their ", 
+            ["FAQ", "https://support.discord.com/hc/en-us/categories/115000168371"],
+            ". Discord supports voice, video, and screen-sharing capabilities."
+          ],
+          "Company attendees are asked to remain in their respective Voice Channels for their dedicated review times, noted on their itineraries, where students will join the voice channel at their designated times. Our volunteers will post reminders in the #time channel when each review has 5 minutes remaining. Students will be expected to leave at the end of their reviews, otherwise volunteers will move students out of the voice channel."
+        ]}
         deg={1.5}
       />
       <FaqComponent 
-        question="How will the virtual Portfolio Reviews work during CoMotion 2021?"
-        answer={["Portfolio reviews will be conducted via the CoMotion 2021 Discord server that attendees can access from their itinerary email and students can access through the MOMELove Club Discord.", "Attendees are asked to hang out in their respective Voice Chats for the portion of the day dedicated to portfolio reviews, whereas students will come in and out of those rooms. Our volunteers will be posting announcements in the #time channel 5 minutes before each review time is over, and will enter the Voice Channel at the end of each review to check in. Our server can handle everyone screensharing, but we recommend having the student’s website pulled up on their end to share their screen."]}
+        question="When and where are my portfolio reviews?"
+        answer={[
+          "Both students and companies should have been received in an email from MOMELove with the subject line: “CoMotion 2022 Portfolio Reviews.” This email will contain a schedule as well as helpful information regarding the review process."
+        ]}
         deg={-0.5}
       />
       <FaqComponent 
-        question="Why are we using Discord for the CoMotion 2021 Portfolio Reviews?"
-        answer={["We are aware that Discord may be a brand new platform to use besides Zoom or Slack, however, due to the capabilities of remote officers and volunteers, Discord’s large-group setting with the ability to break into smaller sections and general accessibility is the best option for overseeing portfolio reviews."]}
+        question="Do I need to set up special accounts for CoMotion 2022?"
+        answer={[
+          [
+            "All companies and students participating in portfolio reviews must create a free Discord account ", 
+            ["here", "https://discord.com/"],
+            ". You do not need an account to view the livestreams or panels hosted on YouTube, however you will need an account to participate in YouTube comments."
+          ]
+        ]}
         deg={2}
       />
     </div>
