03a0f4a7147216496624145ce9bf9fe5d354af3a
Andrew Goodridge <gandrew888@gmail.com>
2022-02-24 13:19:48 -0500

    oops!

--- src/components/ctaGrid.js
+++ src/components/ctaGrid.js
@@ -9,7 +9,7 @@ const CtaGrid = () => {
         <main className="ctaGrid">
             <div className="ctaGrid-container">
                 <Link to="/attendees" className="cta bg3"><p>Attendees Page</p></Link>
-                <Link to="/panels" className="cta bg1"><p>Panels Page</p></Link>
+                <Link to="/livestream" className="cta bg1"><p>Livestream Page</p></Link>
                 <Link to="/faq" className="cta bg2"><p>FAQ Page</p></Link>
                 <Link to="/team" className="cta bg4"><p>Team Page</p></Link>
             </div>
